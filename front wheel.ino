f5e03dac209b93ee63f0fc852bf753d126268177
Adil-Rehan <146543468+AdilRehan1@users.noreply.github.com>
2026-03-30 23:34:04 +0200

    Implement motor control with joystick input
    Added motor driver pins and implemented speed control for forward, reverse, and stop based on joystick input.

--- front wheel.ino
+++ front wheel.ino
@@ -5,7 +5,11 @@ Servo steeringServo;
 // Joystick pins
 int joyX = A0;
 int joyY = A1;
-int servo = 9;
+
+// Motor driver pins
+int IN1 = 5;
+int IN2 = 6;
+int PWM = 3;
 
 // Steering limits
 int minAngle = 30;
@@ -15,7 +19,11 @@ int maxAngle = 150;
 int deadZone = 50;
 
 void setup() {
-  steeringServo.attach(servo);
+  steeringServo.attach(9);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(PWM, OUTPUT);
 }
 
 void loop() {
@@ -34,5 +42,33 @@ void loop() {
   steeringServo.write(angle);
 
   // ---------- THROTTLE ----------
+  int speed;
+
+  if (yVal > (512 + deadZone)) {
+    // FORWARD
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+
+    speed = map(yVal, 512 + deadZone, 1023, 0, 255);
+    analogWrite(PWM, speed);
+
+  } 
+  else if (yVal < (512 - deadZone)) {
+    // REVERSE
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+
+    speed = map(yVal, 512 - deadZone, 0, 0, 255);
+    analogWrite(PWM, speed);
+
+  } 
+  else {
+    // STOP
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    analogWrite(PWM, 0);
+  }
+
   delay(10);
+}  delay(10);
 }
